4eda580f454908e62aab3a7e9f0861d9caee107c
kleinfreund <philipprudloff@fastmail.com>
2023-05-24 19:14:08 +0200

    feat: adds types
    Adds a types/index.d.ts file that will be distributed with the package in future releases.

--- src/get-reverse-iterating-array.js
+++ src/get-reverse-iterating-array.js
@@ -1,4 +1,16 @@
 /**
+ * Returns a `Proxy` to a built-in `Array` whose iteration direction is reversed without affecting the original `Array` object.
+ *
+ * **Example**:
+ *
+ * ```js
+ * const array = [1, 2, 3]
+ * const reverseIteratingArray = getReverseIteratingArray(array)
+ *
+ * Array.from(array) // [ 1, 2, 3 ]
+ * Array.from(reverseIteratingArray) // [ 3, 2, 1 ]
+ * ```
+ *
  * @param {T[]} array
  * @returns {T[]}
  * @template T
